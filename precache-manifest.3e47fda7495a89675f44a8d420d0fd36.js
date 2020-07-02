d7c27d05a48df9929d98c28ab69ddfbf6f8fec80
jamesonbrown <jameson.brown@gmail.com>
2020-07-02 15:38:35 -0400

    Updates

---
+++ precache-manifest.3e47fda7495a89675f44a8d420d0fd36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a016e006b4d5c64e55b65564caa40be4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b701c2c694b5178225a4",
+    "url": "/static/css/main.f17d410c.chunk.css"
+  },
+  {
+    "revision": "ca08cfd95b5b72e13238",
+    "url": "/static/js/2.61f52a34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.61f52a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b701c2c694b5178225a4",
+    "url": "/static/js/main.91aeb53a.chunk.js"
+  },
+  {
+    "revision": "7837c75d956d12bcd112",
+    "url": "/static/js/runtime-main.8eb1d9ee.js"
+  }
+]);
